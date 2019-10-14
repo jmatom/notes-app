f667cf1d45a4ea5636f4cf91685a423bf1580b71
Jose Mato <jose.mato@mobgen.com>
2019-10-14 21:29:35 +0200

    add endpoint to attach tag to a note

--- app/webserver/routes/notes-router.js
+++ app/webserver/routes/notes-router.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const router = require('express').Router();
+const addTagToNote = require('../controllers/notes/add-tag-note-controller');
 const createNote = require('../controllers/notes/create-note-controller');
 const checkAccountSession = require('../controllers/account/check-account-session');
 const deleteNote = require('../controllers/notes/delete-note-controller');
@@ -11,5 +12,6 @@ router.post('/', checkAccountSession, createNote);
 router.get('/', checkAccountSession, getNotes);
 router.get('/:noteId', checkAccountSession, getNote);
 router.delete('/:noteId', checkAccountSession, deleteNote);
+router.post('/:noteId/tags', checkAccountSession, addTagToNote);
 
 module.exports = router;
